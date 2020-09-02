f8876a478b09db43745a88f330097ef600908beb
Jake Mensch <jake.mensch@gmail.com>
2020-09-02 06:46:10 -0700

    moved module aliases into next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,31 @@
+const path = require('path')
+
+//// MODULE ALIASES ////
+
+const resolve = (p) => path.resolve(__dirname, p)
+
+const aliases = {
+  '@components': resolve('./components'),
+  '@config': resolve('./config'),
+  '@constants': resolve('./constants'),
+  '@utils': resolve('./utils'),
+}
+
+//// BUNDLE ANALYZER ////
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer({})
+//// EXPORT ////
+
+module.exports = withBundleAnalyzer({
+  webpack: (config, options) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...aliases,
+    }
+
+    return config
+  },
+})
