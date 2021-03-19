5d4cf6634d0eaf5fa700e9b4f83cb65e54f066db
Joseph Hamman <jhamman@ucar.edu>
2021-03-19 12:15:37 -0700

    try having a dummy index (for azure)

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { Box } from 'theme-ui'
+import Layout from '../components/layout'
+
+function Index() {
+  return (
+    <Layout>
+      <Box>Hello! Please go to /research/forest-risks</Box>
+    </Layout>
+  )
+}
+
+export default Index
